afd2f9c880676e5d48caffa0420622f62f857b90
biancaoliv <bianca.oliv.bn@gmail.com>
2022-11-12 22:06:44 -0300

    add: repositories added and ProductsRepository configured.

--- src/shared/typeorm/data-source.ts
+++ src/shared/typeorm/data-source.ts
@@ -7,8 +7,10 @@ const port = process.env.DB_PORT as number | undefined;
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: port,
-  username: process.env.USER,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.NAME,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
